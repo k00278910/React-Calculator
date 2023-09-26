f37fb985f1ab4516717b4c19eb46b85d54e5f05d
k00278910 <k00278910@student.lit.ie>
2023-09-26 21:48:09 +0100

    add HTML markup to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
 
 
 function App() {
+
+// Function to add Numbers 0-9 on display
+const createDigits =()=>{
+  // array of digits to store array elements
+  const digits = [];
+
+  for (let i=1;i<10;i++){
+
+    digits.push(
+      <button key={i}>{i}</button>
+    )
+
+  }
+  return digits;
+}
+
   return (
     <div className="App">
-      Hello World!
+      {/* add HTML markup for display*/}
+      <div className="App">
+        <div className="calculator">
+          <span>(0)</span>0
+        </div>
+
+        <div className="operators">
+          <button>/</button>
+          <button>*</button>
+          <button>+</button>
+          <button>-</button>
+
+          <button>DEL</button>
+        </div>
+
+        <div className="digits">
+          {/* create buttons 1-9 using a function */}
+          {createDigits()}
+          <button>.</button>
+          <button>=</button>
+        </div>
+
+      </div>
     </div>
   );
 }
